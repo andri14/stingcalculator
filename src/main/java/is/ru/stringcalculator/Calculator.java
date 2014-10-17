7abc5933331903ba40df053ffe1854f78f78e9f2
andri14 <andri14@ru.is>
2014-10-17 17:47:31 +0000

    Added test for two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,10 +3,18 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if(text.equals("")){
-        return 0;
-    	}
-    	else
-        	return 1;
-		}
+		 if(text.equals("")){
+    		return 0;
+			}
+		else if(text.contains(",")){
+   			String[] numbers = text.split(",");
+    		return toInt(numbers[0]) + toInt(numbers[1]);
+			}
+		else
+    		return 1;
+
+	#Refactor
+	private static int toInt(String number){
+    	return Integer.parseInt(number);
+	}
 }
